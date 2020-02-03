5e41e6232f22ed656b12a6038303d69b39c601a4
Vikalp Chakravorty <Vicks548@gmail.com>
2020-02-03 09:44:54 +0530

    Web View in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      appBar: AppBar(
+        title: Text("Flutter WebView"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      url: "https://www.cricbuzz.com/",
+    );
+  }
+}
